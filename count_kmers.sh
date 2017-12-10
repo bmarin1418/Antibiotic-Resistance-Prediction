9850a99b2cf994156afadaee1ff42d573cc8ba72
Kyle <kgifaldi@nd.edu>
2017-12-10 14:20:29 -0500

    changed 30 to 25 kmer

--- count_kmers.sh
+++ count_kmers.sh
@@ -4,6 +4,6 @@ FILES=./sequences/*
 for f in $FILES; do
 	NAME=$(echo "$f" | cut -d'/' -f3)
 	if [ ! -f "./kmer_cnts/$NAME.jf" ]; then
-		jellyfish count -o "./kmer_cnts/$NAME.jf" -m 30 -s 100M -t 8 -C "$f"
+		jellyfish count -o "./kmer_cnts/$NAME.jf" -m 25 -s 100M -t 8 -C "$f"
 	fi
 done
