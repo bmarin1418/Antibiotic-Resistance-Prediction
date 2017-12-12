641d88a5750debf1f2f14db69e389902138de2e9
bmarin <bmarin@nd.edu>
2017-12-11 21:10:12 -0500

    changed to random forrest

--- classifier.py
+++ classifier.py
@@ -20,34 +20,34 @@ print('The numer of kmers is {}'.format(len(kmer_cnts)))
 
 def createMatrix(mat, genome_files):
     for row_i, file_nm in enumerate(genome_files):
+        if row_i > 4:
+            break
         with open('./pen_cnts/' + file_nm, 'r') as fd:
                  kmer_ids = set(fd.readlines()[0].rstrip().split(' '))
                  for k_id in kmer_ids:
                     mat[row_i, int(k_id)] = 1
-        if row_i > 5:
-            break
     return mat
 
 def createLabels(labels, genome_files):    
     for row_i, file_nm in enumerate(genome_files):
+        if row_i > 4:
+            break
         genome_id = file_nm.partition('.PATRIC')[0]
         pheno = truth_dat[truth_dat['genome_id'] == genome_id].iloc[0]['resistant_phenotype']
         if pheno == 'Susceptible' or pheno == 'Intermediate':
              labels[row_i, 0] = 1
-        if row_i > 5:
-            break
     return labels
 
-train_matrix = np.zeros([100, 30000000], dtype=np.int8)
-test_matrix = np.zeros([100,30000000], dtype=np.int8) 
+train_matrix = np.zeros([5, 30000000], dtype=np.int8)
+test_matrix = np.zeros([5,30000000], dtype=np.int8) 
 #dat_matrix = np.zeros([100, len(kmer_cnts)], dtype=np.int8)
 
-train_labels = np.zeros([100,1])
-test_labels = np.zeros([100, 1])
+train_labels = np.zeros([5,1])
+test_labels = np.zeros([5, 1])
 
 random.shuffle(genome_files)
 train_files = genome_files[:100]
-test_files = genome_files[100:200]
+test_files = genome_files[100:150]
 
 truth_dat = pd.read_csv("PATRIC_genomes_AMR.tsv", sep='\t', dtype=str)
 truth_dat = truth_dat[truth_dat['antibiotic'] == 'penicillin']
@@ -58,8 +58,8 @@ train_labels = createLabels(train_labels, train_files)
 test_labels = createLabels(test_labels, test_files)
 
 
-clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1, max_features="sqrt"), algorithm="SAMME")
-
-clf.fit(train_matrix, train_labels)
+clf = RandomForestClassifier(n_jobs=16, max_features="sqrt", n_estimators=50)
+clf.fit(train_matrix, np.ravel(train_labels))
 preds = clf.predict(test_matrix)
-print(preds)
+np.save('preds', preds)
+np.save('labels', np.ravel(test_labels))
